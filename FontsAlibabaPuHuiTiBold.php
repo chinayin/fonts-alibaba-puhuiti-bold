1cac307dc9d587eae9c4cd6a71ac690a415cc07f
lei.tian <whereismoney@qq.com>
2022-02-17 22:44:22 +0800

    first commit

---
+++ FontsAlibabaPuHuiTiBold.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Fonts\AlibabaPuHuiTi\Bold;
+
+class FontsAlibabaPuHuiTiBold
+{
+
+    /**
+     * @return string
+     */
+    public static function getPath(): string
+    {
+        return __DIR__ . 'Alibaba-PuHuiTi-Bold.ttf';
+    }
+
+
+}
