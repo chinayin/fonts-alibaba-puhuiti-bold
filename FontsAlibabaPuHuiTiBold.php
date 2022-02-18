b45af46cb9724cba2c24d3add4589ed044dfada0
lei.tian <whereismoney@qq.com>
2022-02-18 18:21:15 +0800

    修复路径错误

--- FontsAlibabaPuHuiTiBold.php
+++ FontsAlibabaPuHuiTiBold.php
@@ -10,7 +10,7 @@ class FontsAlibabaPuHuiTiBold
      */
     public static function getPath(): string
     {
-        return __DIR__ . 'Alibaba-PuHuiTi-Bold.ttf';
+        return __DIR__ . '/Alibaba-PuHuiTi-Bold.ttf';
     }
 
 
